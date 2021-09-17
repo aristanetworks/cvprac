8a5449c55a735fa712919c6ec0dc38ba8bba84a7
MattH <mhartzel@arista.com>
2021-09-17 08:13:17 -0400

    Formatting fixes

--- docs/labs/lab06-provisioning/vc_task_retrigger.py
+++ docs/labs/lab06-provisioning/vc_task_retrigger.py
@@ -7,26 +7,24 @@
 import argparse
 import ssl
 import sys
-from packaging import version
+from pkg_resources import parse_version
 from getpass import getpass
 from cvprac.cvp_client import CvpClient
-
-
-if ((sys.version_info.major == 3) or
-    (sys.version_info.major == 2 and sys.version_info.minor == 7 and sys.version_info.micro >= 5)):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
 
+if ((sys.version_info.major == 3) or
+        (sys.version_info.major == 2 and sys.version_info.minor == 7 and
+         sys.version_info.micro >= 5)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def main():
         
     compliance = {"0001": "Config is out of sync",
         "0003": "Config & image out of sync",
-#        "0004": "Config, Image and Device time are in sync",
+        "0004": "Config, Image and Device time are in sync",
         "0005": "Device is not reachable",
         "0008": "Config, Image and Extensions are out of sync",
         "0009": "Config and Extensions are out of sync",
@@ -38,14 +36,13 @@ def main():
     # Create connection to CloudVision
     clnt = CvpClient()
 
-
-    parser = argparse.ArgumentParser(description='Script to recreate a task, if a previous config push was cancelled')
+    parser = argparse.ArgumentParser(
+        description='Script to recreate a task, if a previous config push was cancelled')
     parser.add_argument('-u', '--username', default='username')
     parser.add_argument('-p', '--password', default=None)
     parser.add_argument('-c', '--cvpserver', action='append')
     parser.add_argument('-f', '--filter', action='append', default=None)
     args = parser.parse_args()
-
 
     if args.password is None:
         args.password = getpass()
@@ -59,7 +56,7 @@ def main():
 
         # Get the current CVP version
         cvp_release = clnt.api.get_cvp_info()['version']
-        if version.parse(cvp_release) < version.parse('2020.3.0'):
+        if parse_version(cvp_release) < parse_version('2020.3.0'):
             # For older CVP, we manually trigger a compliance check
             try:
                 clnt.api.check_compliance('root', 'container')
@@ -71,7 +68,6 @@ def main():
             # with continuous compliance checks, triggering the check is no longer required
             pass
 
-
         device_filters = []
         if args.filter is not None:
             for entry in args.filter:
@@ -82,13 +78,13 @@ def main():
         devices = clnt.api.get_inventory()
         print("%d devices in inventory" % len(devices) )
 
-        #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
         for switch in devices:
-            if switch['status'] == 'Registered' and switch['parentContainerId'] != 'undefined_container':
+            if (switch['status'] == 'Registered' and
+                    switch['parentContainerId'] != 'undefined_container'):
 
                 if len(device_filters) > 0:
-                # iterate over device filters, and update task for any devices not in compliance
+                    # iterate over device filters, and update task for
+                    # any devices not in compliance
                     
                     for filter_term in device_filters:
                         print("Checking device: %s" % switch['hostname'])
@@ -110,10 +106,10 @@ def main():
                         print(clnt.api.apply_configlets_to_device("", switch, cl))
 
             else:
-                print("Skipping %s, device is unregisted for provisioning" % switch['hostname'])
+                print("Skipping %s, device is unregistered for provisioning" % switch['hostname'])
 
     return 0
 
+
 if __name__ == "__main__":
     main()
-    
