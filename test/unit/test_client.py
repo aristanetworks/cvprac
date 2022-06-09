d064a771ac25ac996c933961ce38cb8b88d5a77a
MattH <mhartzel@arista.com>
2022-06-09 12:58:44 -0400

    Test: Add test cases for JSON Decode handling

--- test/unit/test_client.py
+++ test/unit/test_client.py
@@ -36,7 +36,7 @@
 import unittest
 from itertools import cycle
 from mock import Mock
-from requests.exceptions import HTTPError, ReadTimeout
+from requests.exceptions import HTTPError, ReadTimeout, JSONDecodeError
 from cvprac.cvp_client import CvpClient
 from cvprac.cvp_client_errors import CvpApiError, CvpSessionLogOutError
 
@@ -215,6 +215,248 @@ class TestClient(unittest.TestCase):
         self.assertIsNone(self.clnt.last_used_node)
         self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
         request_return_value.json.assert_called_once_with()
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_no_response(self):
+        """ Test handling of response being empty.
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        self.clnt.session.get.return_value = None
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        resp = self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
+        self.assertIsNone(resp)
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_no_response_content(self):
+        """ Test handling of response content being None.
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        response_mock = Mock()
+        response_mock.content = None
+        response_mock.text = None
+        self.clnt.session.get.return_value = response_mock
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        resp = self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
+        expected_response = {"data": []}
+        self.assertEqual(resp, expected_response)
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_empty_response_content(self):
+        """ Test handling of response content being empty.
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        response_mock = Mock()
+        response_mock.content = b''
+        response_mock.text = ""
+        self.clnt.session.get.return_value = response_mock
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        resp = self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
+        expected_response = {"data": []}
+        self.assertEqual(resp, expected_response)
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_response_content_single_json_object(self):
+        """ Test handling of response being valid single JSON object.
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        response_mock = Mock()
+        response_mock.content = b'{"data":"success"}'
+        response_mock.json.return_value = {"data": "success"}
+        response_mock.text = '{"data":"success"}'
+        self.clnt.session.get.return_value = response_mock
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        resp = self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
+        expected_response = {"data": "success"}
+        self.assertEqual(resp, expected_response)
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_response_content_multi_json_object(self):
+        """ Test handling of response being valid multiple JSON objects for
+            Streaming JSON.
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        response_mock = Mock()
+        response_mock.content = b'{"result":{"value":{' \
+                                b'"key":{"workspaceId":"CVPRAC_TEST",' \
+                                b'"value":"TAGTESTDEV"},' \
+                                b'"remove":false},' \
+                                b'"type":"INITIAL"}}\n' \
+                                b'{"result":{"value":{' \
+                                b'"key":{"workspaceId":"CVPRAC_TEST2",' \
+                                b'"value":"TAGTESTINT"},' \
+                                b'"remove":false},' \
+                                b'"type":"INITIAL"}}\n'
+        response_mock.json.side_effect = JSONDecodeError("Extra data")
+        response_mock.text = '{"result":{"value":{' \
+                             '"key":{"workspaceId":"CVPRACT1",' \
+                             '"value":"T1"},' \
+                             '"remove":false},' \
+                             '"type":"I1"}}\n' \
+                             '{"result":{"value":{' \
+                             '"key":{"workspaceId":"CVPRACT2",' \
+                             '"value":"T2"},' \
+                             '"remove":false},' \
+                             '"type":"I2"}}\n'
+        self.clnt.session.get.return_value = response_mock
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        resp = self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
+        multi_objects = [
+            {"result": {"value": {"key": {"workspaceId": "CVPRACT1",
+                                          "value": "T1"},
+                                  "remove": False},
+                        "type": "I1"}},
+            {"result": {"value": {"key": {"workspaceId": "CVPRACT2",
+                                          "value": "T2"},
+                                  "remove": False},
+                        "type": "I2"}}]
+        expected_response = {"data": multi_objects}
+        self.assertEqual(resp, expected_response)
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_response_content_truncate_long_error(self):
+        """ Test handling of response being valid multiple JSON objects for
+            Streaming JSON with large data that causes for large error message
+            to be truncated
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        response_mock = Mock()
+        response_mock.content = b'{"result":{"value":{' \
+                                b'"key":{"workspaceId":"CVPRAC_TEST",' \
+                                b'"value":"TAGTESTDEV"},' \
+                                b'"remove":false},' \
+                                b'"type":"INITIAL"}}\n' \
+                                b'{"result":{"value":{' \
+                                b'"key":{"workspaceId":"CVPRAC_TEST2",' \
+                                b'"value":"TAGTESTINT"},' \
+                                b'"remove":false},' \
+                                b'"type":"INITIAL"}}\n'
+        long_error = 'Extra data: ' \
+                     '{"result":{"value":{"key":{' \
+                     '"workspaceId":"builtin-studios-v0.82-evpn-services"},' \
+                     '"createdAt":"2022-05-25T23:18:33.204Z",' \
+                     '"createdBy":"aerisadmin",' \
+                     '"lastModifiedAt":"2022-05-25T23:18:33.601Z",' \
+                     '"lastModifiedBy":"aerisadmin",' \
+                     '"state":"WORKSPACE_STATE_SUBMITTED",' \
+                     '"lastBuildId":"build-11b310a6bc5",' \
+                     '"responses":{"values":{' \
+                     '"build-18f4ed17-4f4d-41e6-8091-ad4b310a6bc5":' \
+                     '{"status":"RESPONSE_STATUS_SUCCESS",' \
+                     '"message":"Build build-18f4ed17-10a6bc5 finished' \
+                     ' successfully"},"submit-1":{"status":' \
+                     '"RESPONSE_STATUS_SUCCESS","message":' \
+                     '"Submitted successfully"}}},"ccIds":{},' \
+                     '"type":"INITIAL"}}{"result":{"value":{"key":' \
+                     '{"workspaceId":"builtin-studios1vity-monitor"}' \
+                     ',"createdAt":"2022-05-25T23:18:32.368Z",' \
+                     '"Build bui1sfully"},"}}'
+        response_mock.json.side_effect = JSONDecodeError(long_error)
+        response_mock.text = '{"result":{"value":{' \
+                             '"key":{"workspaceId":"CVPRACT1",' \
+                             '"value":"T1"},' \
+                             '"remove":false},' \
+                             '"type":"I1"}}\n' \
+                             '{"result":{"value":{' \
+                             '"key":{"workspaceId":"CVPRACT2",' \
+                             '"value":"T2"},' \
+                             '"remove":false},' \
+                             '"type":"I2"}}\n'
+        self.clnt.session.get.return_value = response_mock
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        resp = self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
+        multi_objects = [
+            {"result": {"value": {"key": {"workspaceId": "CVPRACT1",
+                                          "value": "T1"},
+                                  "remove": False},
+                        "type": "I1"}},
+            {"result": {"value": {"key": {"workspaceId": "CVPRACT2",
+                                          "value": "T2"},
+                                  "remove": False},
+                        "type": "I2"}}]
+        expected_response = {"data": multi_objects}
+        self.assertEqual(resp, expected_response)
+        self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
+
+    def test_make_request_response_content_incomplete_json_object(self):
+        """ Test handling of response being invalid JSON objects for
+            Streaming JSON.
+        """
+        self.clnt.session = Mock()
+        self.clnt.session.return_value = True
+        response_mock = Mock()
+        response_mock.content = b'{"result":{"value":{' \
+                                b'"key":{"workspaceId":"CVPRAC_TEST",' \
+                                b'"value":"TAGTESTDEV"},' \
+                                b'"remove":false},' \
+                                b'"type":"INITIAL"}}\n' \
+                                b'{"result":{"value":{' \
+                                b'"key":{"workspaceId":"CVPRAC_TEST2",' \
+                                b'"value":"TAGTESTINT"},' \
+                                b'"remove":false},' \
+                                b'"type":"INITIAL"\n'
+        response_mock.json.side_effect = JSONDecodeError("Unknown")
+        response_mock.text = '{"result":{"value":{' \
+                             '"key":{"workspaceId":"CVPRACT1",' \
+                             '"value":"T1"},' \
+                             '"remove":false},' \
+                             '"type":"I1"}}\n' \
+                             '{"result":{"value":{' \
+                             '"key":{"workspaceId":"CVPRACT2",' \
+                             '"value":"T2"},' \
+                             '"remove":false},' \
+                             '"type":"I2"\n'
+        self.clnt.session.get.return_value = response_mock
+        self.clnt._create_session = Mock()
+        self.clnt.NUM_RETRY_REQUESTS = 2
+        self.clnt.connect_timeout = 2
+        self.clnt.node_cnt = 2
+        self.clnt.url_prefix = 'https://1.1.1.1:7777/web'
+        self.clnt._is_good_response = Mock(return_value='Good')
+        self.assertIsNone(self.clnt.last_used_node)
+        with self.assertRaises(JSONDecodeError):
+            self.clnt._make_request('GET', 'url', 2, {'data': 'data'})
         self.assertEqual(self.clnt.last_used_node, '1.1.1.1')
 
     def test_make_request_timeout(self):
