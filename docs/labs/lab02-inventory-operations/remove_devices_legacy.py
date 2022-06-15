3026709afd5e5d8a409ea9f437bc171bcfb467a6
Tamas Plugor <41957075+noredistribution@users.noreply.github.com>
2022-06-15 17:25:31 +0100

    Example: Update examples (#205)

--- docs/labs/lab02-inventory-operations/remove_devices_legacy.py
+++ docs/labs/lab02-inventory-operations/remove_devices_legacy.py
@@ -18,4 +18,13 @@ clnt.connect(nodes=['cvp1'], username='', password='', api_token=token)
 devices = ["50:08:00:a7:ca:c3","50:08:00:b1:5b:0b","50:08:00:60:c6:76",
            "50:08:00:25:9d:36","50:08:00:8b:ee:b1","50:08:00:8c:22:49"]
 
+# Remove devices from provisioning
+# This is a legacy API call that removes the devices from Network Provisioning 
+# in CVP versions older than 2021.3.0, however it does not remove them from 
+# the Device Inventory as that requires the streaming agent (TerminAttr) to be shutdown,
+# which this API does not support.
+# To fully decommission a device the device_decommissioning() API can be used, which is
+# supported from 2021.3.0+.
+# Note that using the delete_devices() function post CVP 2021.3.0 the device will be 
+# automatically added back to the Undefined container.
 clnt.api.delete_devices(devices)
