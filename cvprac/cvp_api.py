4b80edf9c489181a02a501440c2d19e0359c728b
Tamas Plugor <tamas@arista.com>
2022-12-14 22:07:59 +0000

    Fix: Use status endpoint instead of config endpoint for service account resource APIs

--- cvprac/cvp_api.py
+++ cvprac/cvp_api.py
@@ -3875,7 +3875,7 @@ class CvpApi(object):
             return self.clnt.post(url, data=payload)
 
     def svc_account_get_all(self):
-        ''' Get all service account states using Resource APIs.
+        ''' Get all service account config/status states using Resource APIs.
             Supported versions: CVP 2021.3.0 or newer and CVaaS.
             Returns:
                 response (list): Returns a list of dictionaries that contains...
@@ -3886,12 +3886,12 @@ class CvpApi(object):
         '''
         msg = 'Service Account Resource APIs are supported from 2021.3.0+.'
         if self.cvp_version_compare('>=', 7.0, msg):
-            url = '/api/v3/services/arista.serviceaccount.v1.AccountConfigService/GetAll'
+            url = '/api/v3/services/arista.serviceaccount.v1.AccountService/GetAll'
             self.log.debug('v7 {} '.format(url))
             return self.clnt.post(url)
 
     def svc_account_get_one(self, username):
-        ''' Get a service account's state using Resource APIs
+        ''' Get a service account's config/status state using Resource APIs
             Supported versions: CVP 2021.3.0 or newer and CVaaS.
             Args:
                 username (string): The service account username.
@@ -3904,7 +3904,7 @@ class CvpApi(object):
         msg = 'Service Account Resource APIs are supported from 2021.3.0+.'
         if self.cvp_version_compare('>=', 7.0, msg):
             payload = {"key": {"name": username}}
-            url = '/api/v3/services/arista.serviceaccount.v1.AccountConfigService/GetOne'
+            url = '/api/v3/services/arista.serviceaccount.v1.AccountService/GetOne'
             self.log.debug('v7 {} {}'.format(url, payload))
             return self.clnt.post(url, data=payload)
 
